abc4bec6cf9fb125159eb8c092d0ed3caa01d4c0
thodoris roumeliotis <troumel@hotmail.com>
2025-11-01 22:57:00 +0200

    added path parameters file

--- path_parameters.py
+++ path_parameters.py
@@ -3,21 +3,22 @@ from enum import Enum
 
 app = FastAPI()
 
+
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
 
+
 @app.get("/items/{item_id}")
 def read_item(model_name: ModelName):
     if model_name == ModelName.alexnet:
         return {"model": ModelName.alexnet}
-    
+
     if model_name == ModelName.resnet:
         return {"model": ModelName.resnet}
-    
+
     if model_name == ModelName.lenet:
         return {"model": ModelName.lenet}
 
     return {"model": "unknown"}
-
